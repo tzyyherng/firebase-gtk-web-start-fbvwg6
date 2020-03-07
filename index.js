c00065b370c69d5c8dd63a36b6c0ac167c680f37
tzyyherng <tzyyherng@gmail.com>
2020-03-07 11:54:32 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,118 @@
+
+import './style.css';
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+import * as firebaseui from 'firebaseui';
+
+
+// Document elements
+const startRsvpButton = document.getElementById('startRsvp');
+const guestbookContainer = document.getElementById('guestbook-container');
+
+const form = document.getElementById('leave-message');
+const input = document.getElementById('message');
+const guestbook = document.getElementById('guestbook');
+const numberAttending = document.getElementById('number-attending');
+const rsvpYes = document.getElementById('rsvp-yes');
+const rsvpNo = document.getElementById('rsvp-no');
+
+var rsvpListener = null;
+var guestbookListener = null;
+
+// Add Firebase project configuration object here
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAT2XTV81NyCNlS8gBltfCRmRj4jb56ONc",
+    authDomain: "fir-study-jam-afdee.firebaseapp.com",
+    databaseURL: "https://fir-study-jam-afdee.firebaseio.com",
+    projectId: "fir-study-jam-afdee",
+    storageBucket: "fir-study-jam-afdee.appspot.com",
+    messagingSenderId: "1073137429316",
+    appId: "1:1073137429316:web:abf066ec36c9d7bea1c511"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+// FirebaseUI config
+const uiConfig = {
+  credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+  signInOptions: [
+    // Email / Password Provider.
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+  callbacks: {
+    signInSuccessWithAuthResult: function(authResult, redirectUrl){
+      // Handle sign-in.
+      // Return false to avoid redirect.
+      return false;
+    }
+  }
+};
+
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+// Called when the user clicks the RSVP button
+startRsvpButton.addEventListener("click",
+ () => {
+    if (firebase.auth().currentUser) {
+      // User is signed in; allows user to sign out
+      firebase.auth().signOut();
+    } else {
+      // No user is signed in; allows user to sign in
+      ui.start("#firebaseui-auth-container", uiConfig);
+    }
+});
+
+// Listen to the current Auth state
+firebase.auth().onAuthStateChanged((user) => {
+if (user){
+  startRsvpButton.textContent = "LOGOUT";
+  // Show guestbook to logged-in users
+  guestbookContainer.style.display = "block";
+
+  // Subscribe to the guestbook collection
+  subscribeGuestbook();
+}
+else{
+  startRsvpButton.textContent = "RSVP";
+  // Hide guestbook for non-logged-in users
+  guestbookContainer.style.display = "none";
+
+  // Unsubscribe from the guestbook collection
+  unsubscribeGuestbook();
+}
+});
+
+// Listen to the form submission
+function subscribeGuestbook(){
+   // Create query for messages
+ guestbookListener = firebase.firestore().collection("guestbook")
+ .orderBy("timestamp","desc")
+ .onSnapshot((snaps) => {
+   // Reset page
+   guestbook.innerHTML = "";
+   // Loop through documents in database
+   snaps.forEach((doc) => {
+     // Create an HTML entry for each document and add it to the chat
+     const entry = document.createElement("p");
+     entry.textContent = doc.data().name + ": " + doc.data().text;
+     guestbook.appendChild(entry);
+   });
+ });
+};
+
+
+function unsubscribeGuestbook(){
+ if (guestbookListener != null)
+ {
+   guestbookListener();
+   guestbookListener = null;
+ }
+};
+
+
